19b5fde2159769e5e4ec4bd5d19a41c615811e32
kusano <kusano@techfun.jp>
2024-11-14 20:54:55 +0900

    仮でS3バケットとDynamoDBテーブルだけ追加

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -12,5 +12,28 @@ export class InfraStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'InfraQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const s3 = new cdk.aws_s3.Bucket(this, 'sd-images', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+    });
+
+    const sdDynamoTable = new cdk.aws_dynamodb.Table(this, 'sd', {
+      partitionKey: { name: 'locationId', type: cdk.aws_dynamodb.AttributeType.STRING },
+      sortKey: { name: 'category', type: cdk.aws_dynamodb.AttributeType.STRING },
+      tableName: 'SatelliteData',
+      billingMode: cdk.aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const personaDynamoTable = new cdk.aws_dynamodb.Table(this, 'persona', {
+      partitionKey: { name: 'personaId', type: cdk.aws_dynamodb.AttributeType.STRING },
+      tableName: 'Persona',
+      billingMode: cdk.aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
